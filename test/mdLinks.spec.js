4413c205c85a95f0a446dbee477968269f07ab9f
YocelinGRomero <garcia_romero.y@hotmail.com>
2018-09-02 19:00:21 -0500

    Agregando test

---
+++ test/mdLinks.spec.js
@@ -0,0 +1,7 @@
+const {mdLinks} = require('../lib/mdLinks');
+describe('mdLiks es una función del modulo', () => {
+	it('mdLinks es una funcion', (done) => {
+		expect(true).toBe(true);
+		done();
+	});
+});
