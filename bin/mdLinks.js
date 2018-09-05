bca2884b7d525c77ec8c513c9506a050170ecd94
YocelinGRomero <garcia_romero.y@hotmail.com>
2018-09-05 15:58:41 -0500

    Añadiendo texto de cada url al arreglo de objetos de retorno

--- bin/mdLinks.js
+++ bin/mdLinks.js
@@ -2,20 +2,22 @@ const fs = require('fs'); // Accede al CRUD de archivos desde node.js
 const fetch = require('node-fetch'); // Requerir fetch para peticiones a urls
 
 // Realiza las peticiones fetch a las urls para comprobar su estado
-const fetchRequest = (url) => {
+const fetchRequest = (text, url) => {
 	let urlsArray = []; 
 	fetch(url)
 		.then(res => {
 			if (res.status === 404) {
 				urlsArray.push({ // Retorna objeto con error 
 					url: url,
-					status: 'Error, url rota'
+					status: 'Error, url rota',
+					texto: text
 				});
 			}
 			else { 
 				urlsArray.push({ // Retorna objeto con resp exitosa
 					url: url,
-					status: 'Ok, url activa'  
+					status: 'Ok, url activa',
+					textto: text  
 				});
 			}
 			return urlsArray;
@@ -28,10 +30,10 @@ const fetchRequest = (url) => {
 		});
 };
 // Realiza las peticiones fetch por cada url del arreglo existente
-const httpPetitions = (array) => {
+const httpPetitions = (arrayText, arrayURL) => {
 	let peticiones = [];
-	for (let i=0; i<array.length; i++) {
-		peticiones[i] = fetchRequest(array[i]);
+	for (let i=0; i<arrayURL.length; i++) {
+		peticiones[i] = fetchRequest(arrayText[i].toString(), arrayURL[i]);
 	}
 };
 // Separa linea por linea del doc .md y busca concidencias con urls
@@ -41,10 +43,18 @@ const separeteLines = (stringData) => {
 	let lineWithUrl = '';
 	let aux = '';
 	let aux2 = '';
+	let auxText ='';
+	let textFromURL = [];
 	for (let i=0; i<lines.length;i++){
 		lineWithUrl = lines[i].match(/(ftp|http|https|www):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/gi);
 		if(lineWithUrl == null){
 		} else {
+			auxText = lines[i].match(/\[([^\]]+)]/g);
+			if(auxText == null){
+				textFromURL.push('Texto sin URL');
+			} else {
+				textFromURL.push(auxText);
+			}
 			aux = lineWithUrl.toString();
 			if(aux[aux.length-1] == ')'){
 				aux2 = aux.slice(0, -1);
@@ -60,8 +70,9 @@ const separeteLines = (stringData) => {
 		lineWithUrl = '';
 		aux = '';
 		aux2 = '';
+		auxText = '';
 	}
-	httpPetitions(documentURL);
+	httpPetitions(textFromURL, documentURL);
 };
 // Abre el documento y pide analizarlo
 const goInDocument = (err, data) => {
@@ -72,7 +83,7 @@ const goInDocument = (err, data) => {
 	}
 };
 // Pasa documento,función y formato a la llamada fs readfile
-fs.readFile('README.md', 'utf-8', goInDocument);
+fs.readFile('../README2.md', 'utf-8', goInDocument);
 
 /* Funcion a exportar 
 const mdLinksYGR = (path, options) => {
